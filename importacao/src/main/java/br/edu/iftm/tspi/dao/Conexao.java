10f7aba24354da8a842ab3c42218a6522bfb76c6
rafael-devbot <142061101+rafael-iftm@users.noreply.github.com>
2024-09-12 10:04:42 -0300

    [sonarlint(java:S6437)] refactor: use environment variables for database credentials to enhance security

--- importacao/src/main/java/br/edu/iftm/tspi/dao/Conexao.java
+++ importacao/src/main/java/br/edu/iftm/tspi/dao/Conexao.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 
 public class Conexao {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/dbImportacao";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
+    private static final String URL = System.getenv("DB_URL");
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     // Construtor privado para evitar instanciação
     private Conexao() {
