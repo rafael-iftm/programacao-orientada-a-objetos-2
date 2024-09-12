c699cd420d01fbebd19adcbb6fef9373d3f73de0
rafael-devbot <142061101+rafael-iftm@users.noreply.github.com>
2024-09-12 10:02:49 -0300

    [sonarlint(java:S1118)] refactor: add private constructor to Conexao class to prevent instantiation

--- importacao/src/main/java/br/edu/iftm/tspi/dao/Conexao.java
+++ importacao/src/main/java/br/edu/iftm/tspi/dao/Conexao.java
@@ -6,13 +6,15 @@ import java.sql.DriverManager;
 public class Conexao {
 
     private static final String URL = "jdbc:mysql://localhost:3306/dbImportacao";
-
     private static final String USER = "root";
-
     private static final String PASSWORD = "root";
+
+    // Construtor privado para evitar instanciação
+    private Conexao() {
+        throw new UnsupportedOperationException("Esta classe não pode ser instanciada");
+    }
 
     public static Connection getConnection() throws Exception {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
-
 }
